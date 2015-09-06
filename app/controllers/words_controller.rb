da556f02241ff3fd4013a2d0e332495211f6dd76
4amic <4amicosta@gmail.com>
2015-09-05 20:07:03 -0700

    Adding a page to add new words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,7 +3,13 @@ get '/words' do
     erb :"/words/index"
 end
 
+get '/words/new' do
+	@word = Word.new
+	erb :"/words/new"
+end
+
 get '/words/:id' do
 	@word = Word.find(params[:id])
+	#@anagrams = Word.find_anagrams(@word.text)
 	erb :"/words/show"
 end
