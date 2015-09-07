92964c07b55281412f3be53117f439ea36b56bf7
4amic <4amicosta@gmail.com>
2015-09-07 10:15:39 -0700

    Adding a bigger dictionary file and seed data

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,22 +1,9 @@
 require 'sinatra'
 
-def three_letters(input)
-  if input.length > 3
-    raise Exception.new("Word must be less than or equal to 3 characters.")
-  end
-end
-
-def distinct_letters(input)
-  letter_array = input.chars
-  unique_letters = letter_array.uniq
-  if unique_letters.length < letter_array.length
-    raise Exception.new("Word must have distinct letters please!")
-  end
-end
-
 def valid_input(input)
-  three_letters(input)
-  distinct_letters(input)
+  if input.length < 2
+      raise Exception.new("Word must be 2 or more characters long.")
+  end
 end
 
 get '/' do
